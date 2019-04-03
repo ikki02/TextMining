1af31c4e7645cc57348eaf4994a1ade4b7d455f0
ikki02 <B1200315@gl.aiu.ac.jp>
2019-04-03 22:53:58 +0900

    refactor: .gitignore

--- StudyNote/chicken/ch02/sec2-2-1_first-program-sample.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# 2.2.2節 プログラムの作成
-print('Hello World')
--- StudyNote/chicken/ch02/sec2-5-4_twitter-data-access.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# 2.5.4節  ツイッターのデータ　　ツイッターデータのアクセス方法
-from nltk.twitter import Twitter
-tw = Twitter()
-tw.tweets(keywords='happy', limit=10)
